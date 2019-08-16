7a6ec8cbe1f1fc79ca8948022a4d739cb44cf9fb
Nawfal Ali <nawfalrezo@gmail.com>
2019-08-16 15:11:28 +1000

    server.js with 2 ops add&sub added

---
+++ server.js
@@ -0,0 +1,23 @@
+let express=require('express');
+
+let app=express();
+
+app.get('/add/:no1/:no2',function(req,res){
+    let n1=parseInt(req.params.no1);
+    let n2=parseInt(req.params.no2);
+
+    let result=n1+n2;
+
+    res.send("The output="+result);
+});
+
+app.get('/sub',function(req,res){
+    let n1=parseInt(req.query.no1);
+    let n2=parseInt(req.query.no2);
+
+    let result=n1-n2;
+
+    res.send("The output="+result);
+})
+
+app.listen(8080);
